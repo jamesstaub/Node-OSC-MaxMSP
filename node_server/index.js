9c366b7855b01527f3a96c27de635ea3f54d37e4
jamesstaub <james.staub@gmail.com>
2016-01-23 20:43:36 -0500

    moves keys to ENV

--- node_server/index.js
+++ node_server/index.js
@@ -1,16 +1,15 @@
+'use strict';
 var http = require('http');
-
 var httpPort = 8080;
-var oscIO = require('./oscIO');
+
 var Firebase = require("firebase");
 var firebaseOSC = new Firebase('https://rubberhacks.firebaseio.com/');
 
-// send image
+var oscIO = require('./oscIO');
+
+// amazon s3 bucket
 var streamingS3 = require('streaming-s3'),
     fs = require('fs');
-
-
-
 
 // http server
 function handleRequest(request, response){
@@ -25,45 +24,47 @@ server.listen(httpPort, function(){
 });
 
 firebaseOSC.on("value", function(snapshot) {
-  fbResponse = snapshot.val();
+  var fbResponse = snapshot.val();
   for (var uid in fbResponse) {
     // var userIndex = uidToInt(uid);
     var userData = fbResponse[uid];
 
     for(var slider in userData){
       // oscIO.send(userIndex[uid], Number(userData[slider]));
-
       oscIO.send(String(uid), [slider, Number(userData[slider])]);
     }
   }
 });
 
-var userIndexes = {};
+// var userIndexes = {};
 
-function uidToInt(uid, userIndexeObj) {
-   //convert firbase user ids into a hash of numeric indexes
-  // userIndexes[uid] = true;
-  if (userIndexes.hasOwnProperty(uid)) {
-  }
-  for (var user in userIndexes) {
-    // userIndexes[user] = count;
-    // count ++;
-  }
-  return userIndexes;
+// function uidToInt(uid, userIndexeObj) {
+//    //convert firbase user ids into a hash of numeric indexes
+//   // userIndexes[uid] = true;
+//   if (userIndexes.hasOwnProperty(uid)) {
+//   }
+//   for (var user in userIndexes) {
+//     // userIndexes[user] = count;
+//     // count ++;
+//   }
+//   return userIndexes;
+// }
 
-}
+// amazon s3 bucket
+var fStream = fs.createReadStream(  __dirname+'/../media/img.jpg');
 
+const accessKey = process.env.S3_ACCESS_KEY;
+const secret = process.env.S3_SECRET;
 
-var fStream = fs.CreateReadStream( './clips/capture.mov');
-console.log('fstream' fStream);
-
-var uploader = new streamingS3(fStream, {accessKeyId: 'accessKey', secretAccessKey: 'secretKey'},
+var uploader = new streamingS3(fStream, {
+  accessKeyId: accessKey,
+  secretAccessKey: secret},
   {
-    Bucket: 'example.streaming-s3.com',
-    Key: 'video.mp4',
-    ContentType: 'video/mp4'
+    Bucket: 'nodeoscjitter',
+    Key: 'img.jpg',
+    ContentType: 'image/jpeg'
   },  function (err, resp, stats) {
-  if (err) return console.log('Upload error: ', e);
+  if (err) return console.log('Upload error: ', err);
   console.log('Upload stats: ', stats);
   console.log('Upload successful: ', resp);
   }
